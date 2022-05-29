420695d9514942809c372a2ea8641479bfb6f7cd
이진영 <ijin-yeong@ijin-yeong-ui-MacBookPro.local>
2022-05-29 22:08:34 +0900

    Initial commit

---
+++ src/main/java/com/example/ChoosingForYou/ChoosingForYouApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChoosingForYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoosingForYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoosingForYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChoosingForYou/ChoosingForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChoosingForYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoosingForYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
